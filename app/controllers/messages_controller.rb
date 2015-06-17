9f80eaf5c780d5226462381eaa9cbec1a102e5b2
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-17 11:41:27 -0600

    added basic create action in message controller also removed silly single quotes in gemfile

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,11 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
   end
+
+  def create
+    message = Message.create(body: params[:message][:body])
+    respond_to do |format|
+      format.js { render json: { message: message } }
+    end
+  end
 end
